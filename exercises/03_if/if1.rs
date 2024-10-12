5f4ef9b8c98fef4f3d7861b891a70a2637f18fa8
Mac <mac@Macs-MacBook-Pro.local>
2024-10-11 19:59:32 -0600

    Finished until ifs exercises

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,14 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        return a;
+    } 
+    b
 }
 
 fn main() {
-    // You can optionally experiment here.
+
 }
 
 // Don't mind this for now :)
