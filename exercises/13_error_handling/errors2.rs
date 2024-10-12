664ca68d4be75e716f7384b0ef176e8e2c94dc9d
tonyre4 <tonyre4@gmail.com>
2024-10-12 16:48:56 -0600

    finished including errors4

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -22,8 +22,11 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
     // TODO: Handle the error case as described above.
     let qty = item_quantity.parse::<i32>();
+    if qty.is_err(){
+        return qty;
+    }
 
-    Ok(qty * cost_per_item + processing_fee)
+    Ok(qty.unwrap() * cost_per_item + processing_fee)
 }
 
 fn main() {
